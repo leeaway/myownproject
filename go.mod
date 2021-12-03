f5a88699601edc0dc7ee3c4fa2047c1ce6645a57
lijianwei <lijianwei@shopee.com>
2021-12-03 11:27:58 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module example.com/m/v2
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/denisenkom/go-mssqldb v0.11.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/now v1.1.3 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	var user []*User
+	if errs := db.Table("user_tab").Where("id = ?", 1).Find(&user).GetErrors(); len(errs) > 0 {
+		fmt.Println(errs)
+	}
+	fmt.Println(user)
 }
 
+//gorm:
+var (
+	//变量db 通过init直接初始化
+	db     *gorm.DB
+	err    error
+	dbinfo string
+)
+
+func init() {
+	//拼接数据库连接信息
+	dbinfo = fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "52991314wqq", "localhost", "3306", "goentrytask")
+
+	//初始化db
+	db, err = gorm.Open("mysql", dbinfo)
+	if err != nil {
+		fmt.Println("mysql打开失败", err)
+		return
+	}
+}
+
+type User struct {
+	ID       int64  `gorm:"column:id" json:"id"`
+	Unmae    string `gorm:"uname" json:"unmae"`
+	Nickname string `gorm:"nickname" json:"nickname"`
+}
